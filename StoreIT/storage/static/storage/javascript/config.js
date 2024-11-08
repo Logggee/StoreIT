77dad6185b307215fde4a064376f151a67297811
Jan Holderied <holderja@hs-albsig.de>
2024-11-08 12:45:47 +0100

    show table only after selecting a radio

--- StoreIT/storage/static/storage/javascript/config.js
+++ StoreIT/storage/static/storage/javascript/config.js
@@ -224,7 +224,8 @@ function createGrayLine() {
 
 // Function for axaj call to get all items that are stored in a specific bin
 function get_all_items_of_bin(bin_id) {
-    console.log("Bin number: " + bin_id);
+    table_container = document.getElementById("container-table");
+    table_container.classList = "container";
 
     fetch(`/config/${bin_id}`)
         .then(response => response.json())
