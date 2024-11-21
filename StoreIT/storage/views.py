8f7f3689e135fa4d0f12aeb914439d8ca53a3469
Jan Holderied <holderja@hs-albsig.de>
2024-11-21 16:28:34 +0100

    config page now extends main template

--- StoreIT/storage/views.py
+++ StoreIT/storage/views.py
@@ -327,7 +327,8 @@ def config(request):
             storages_and_bins[storage] = storage.all_bins_sorted_in_rows()
         print(f"Storaged and all bin: {storages_and_bins}")
         content = {"storage_layout_form": Storage_Layout_Form(),
-                   "storages_and_bins": storages_and_bins}
+                   "storages_and_bins": storages_and_bins,
+                   "current_user": request.user}
         
         return render(request, "storage/configStorage.html", content)
     
